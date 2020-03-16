71c51c8251c22773d8a7d6aa46b31b717926e406
PawelJadach <pawel.jadach.96@gmail.com>
2020-03-16 15:43:51 +0100

    add frontend

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+var cors = require('cors')
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const seatsRoutes = require('./routes/seats.routes');
