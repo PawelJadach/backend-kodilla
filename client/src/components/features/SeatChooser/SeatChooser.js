9e4c7420b2bc594e5484f6ae266fab7bd59da8ba
PawelJadach <pawel.jadach.96@gmail.com>
2020-03-19 07:57:58 +0100

    add free place count

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -9,30 +9,33 @@ import io from 'socket.io-client';
 class SeatChooser extends React.Component {
 
   state = {
-    interval : null,
+    reserved: 0,
   }
 
   
 
   componentDidMount() {
     this.socket = io(`http://localhost:${process.env.PORT || 8000}`);
-    console.log(this.socket);
     const { loadSeats, loadSeatsData } = this.props;
     loadSeats();
-    this.setState({
-      interval: setInterval(loadSeats, 120000),
-    })
     this.socket.on('seatsUpdated', (seats) => loadSeatsData(seats));
   }
 
-  componentWillUnmount(){
-    clearInterval(this.state.interval)
-  }
 
   isTaken = (seatId) => {
     const { seats, chosenDay } = this.props;
+    const isTaken = (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
+    
+    return isTaken;
+  }
 
-    return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
+  freePlaceCount = () => {
+    const { seats, chosenDay } = this.props;
+    let freePlaced = 50;
+    seats.map(seat => {
+      if(seat.day === chosenDay) freePlaced--;
+    }) 
+    return freePlaced
   }
 
   prepareSeat = (seatId) => {
@@ -40,15 +43,15 @@ class SeatChooser extends React.Component {
     const { isTaken } = this;
 
     if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
-    else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
+    else if(isTaken(seatId))return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
+    
   }
 
   render() {
 
     const { prepareSeat } = this;
     const { requests } = this.props;
-
     return (
       <div>
         <h3>Pick a seat</h3>
@@ -57,6 +60,7 @@ class SeatChooser extends React.Component {
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+        <p>Free seats: {this.freePlaceCount()}/50</p>
       </div>
     )
   };
